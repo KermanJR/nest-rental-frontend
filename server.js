e955bdc71002ba2926ddec65564fbd6dc7a79c42
Kerman Pereira <kermanpereira@gmail.com>
2022-08-05 15:47:09 -0300

    netlify commit

--- server.js
+++ server.js
@@ -20,19 +20,6 @@ app.use('*',
     )
 )
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./build/index.html"));
-    });
-  }
-  
-  else {
-    app.use(express.static(path.join(__dirname, '/public')));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, ".public/index.html"));
-    });
-  }
 
 app.listen(process.env.PORT || 3000, (err)=>{
     if(err){
