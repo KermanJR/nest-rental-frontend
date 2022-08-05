b692e3d1a2b808fde1b4bb9654a0563b994fb9ec
Kerman Pereira <kermanpereira@gmail.com>
2022-08-05 15:34:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('*',
 )
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'));
+    res.sendFile(path.join(__dirname, './build/index.html'));
   });
 
 app.listen(process.env.PORT || 3000, (err)=>{
