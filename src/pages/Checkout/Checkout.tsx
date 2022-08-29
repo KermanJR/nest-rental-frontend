5abbbbed152dd7b7c8caf7a89efb5dc20be2be96
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2022-08-29 12:12:42 -0400

    Update Checkout.tsx

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -110,25 +110,20 @@ export const Checkout = () =>{
             })
             let response = await fetchGenerateDocument;
             let json = await response.json();
-            window.localStorage.setItem('key_signature', '')
-         
-                console.log(json);
-                setSignKey(json.request_signature_key);
-                window.localStorage.setItem('key_signature', json.request_signature_key);
-                //setLoading(false)
+            let key = json.request_signature_key;
+            window.localStorage.setItem('key_signature', key);
+            setSignKey(key);
+            console.log("Chave do documento: " + key);
 
-                //Envia lead para o Zoho CRM
-                sendLead();
+            //Envia lead para o Zoho CRM
+            sendLead();
 
-                //Navega para a página de contrato
-                navigate('/contrato', {
-                    state: {
-                        token: json.request_signature_key
-                    }
-                });
-            
-            
-            
+            //Navega para a página de contrato
+            navigate('/contrato', {
+                state: {
+                    token: key
+                }
+            });
             
         }catch(err){
             console.log(err);
