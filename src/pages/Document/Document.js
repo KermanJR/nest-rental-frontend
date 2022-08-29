7f85ddfc7426faca20afac7a5b514655c5f552e8
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2022-08-29 11:57:59 -0400

    Update Document.js

--- src/pages/Document/Document.js
+++ src/pages/Document/Document.js
@@ -7,7 +7,7 @@ import { useLocation } from "react-router-dom";
 
 export const Document = () =>{
     const [tokenNav, setTokenNav] = React.useState('');
-    //const key_signature = window.localStorage.getItem('key_signature'); 
+    const key_signature = window.localStorage.getItem('key_signature'); 
     const { state } = useLocation();
     const { token } = state;
     //setTokenNav(token);
@@ -24,9 +24,9 @@ export const Document = () =>{
     function Clicksign(i){"use strict";function n(n){var t;(e[(t=n).name||t]||[]).forEach(function(t){t(n.data)})}var o,r,t=window.location.protocol+"//"+window.location.host,e={},u=function(t){n(t.data)};return{endpoint:"https://app.clicksign.com",origin:t,mount:function(t){var n="/sign/"+i,e="?embedded=true&origin="+this.origin,e=this.endpoint+n+e;return r=document.getElementById(t),(o=document.createElement("iframe")).setAttribute("src",e),o.setAttribute("style","width: 100%; height: 100%;"),o.setAttribute("allow","camera"),window.addEventListener("message",u),r.appendChild(o)},unmount:function(){return o&&(r.removeChild(o),o=r=null,window.removeEventListener("message",n)),!0},on:function(t,n){return e[t]||(e[t]=[]),e[t].push(n)},trigger:n}}
 
     function run(){
-       // var request_signature_key = input.value;
+        var request_signature_key = input.value;
         if(widget){widget.unmount();}
-        widget = new Clicksign(token);
+        widget = new Clicksign(request_signature_key);
 
         widget.endpoint = 'https://sandbox.clicksign.com';
         widget.origin = 'https://nest-rental.herokuapp.com';
@@ -49,7 +49,7 @@ export const Document = () =>{
     return(
         <>
             <div>
-                <input id='request_signature_key' value={token} style={{display: 'none'}}/>
+                <input id='request_signature_key' value={key_signature} style={{display: 'none'}}/>
             </div>
 
             <div id='container' style={{height: "600px"}}></div>
