5fca098505895a5d77afea2f9902a3ab7e6eb26c
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2022-09-16 18:23:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,29 @@ import { Document } from "./pages/Document/Document";
 import { Login } from "./pages/Login/Login";
 
 function App() {
+  let location = window.location.href;
   return (
     <>
       <BrowserRouter>
       <CheckoutContext>
-        <Header/>
+        {location === "https://nest-rental.herokuapp.com/login" || location === "https://nest-rental.herokuapp.com/login/cadastro"? 
+        <Routes>
+          <Route path="/login/*" element={<Login/>}/>
+        </Routes>
+        :
+        <>
+          <Header/>
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="produto/ecolift-50" element={<SingleProduct/>} />
             <Route path="demonstracao" element={<Demonstration/>} />
             <Route path="checkout" element={<Checkout/>} />
             <Route path="contrato" element={<Document/>} />
-            <Route path="/login/*" element={<Login/>}/>
           </Routes>
           <Footer/>
-        </CheckoutContext>
+        </> 
+      }
+      </CheckoutContext>
       </BrowserRouter>
     </>
 
