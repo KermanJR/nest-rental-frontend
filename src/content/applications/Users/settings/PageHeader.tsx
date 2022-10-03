6cb79256412160d240b2210b67f5dc46f5ac8b22
Fernanda Alves Fernandes <53227747+FeFernandes@users.noreply.github.com>
2022-10-03 12:21:21 -0300

    Added user data

--- src/content/applications/Users/settings/PageHeader.tsx
+++ src/content/applications/Users/settings/PageHeader.tsx
@@ -1,18 +1,20 @@
 import { Typography } from '@mui/material';
 
 function PageHeader() {
-  const user = {
-    name: 'Fernanda Alves',
-    avatar: '/static/images/avatars/1.jpg'
-  };
-
+  const user = window.localStorage.getItem('user');
+  let name = ''
+  let avatar = ''
+  if(user) {
+    name = JSON.parse(user).name 
+    name = name? name : ''
+  }
   return (
     <>
       <Typography variant="h3" component="h3" gutterBottom>
         Configurações da Conta
       </Typography>
       <Typography variant="subtitle2">
-        {user.name}, você pode configurar seus dados!
+        {name}, você pode configurar seus dados!
       </Typography>
     </>
   );
